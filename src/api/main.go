2466bede5053b77133ffc92a4fcf4e69f7ec4fa0
Sublime Tshimpangila <tshimangasublime@gmail.com>
2026-01-07 01:46:56 -0500

    feat: add pg container and gorm to communicate with it

--- src/api/main.go
+++ src/api/main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+const dns = "host=db user=cinema_manager password=cinema_manager dbname=cinema_manager port=5432 sslmode=disable"
+
+type Product struct {
+	gorm.Model
+	Code string
+	Price uint
+}
+
 func main() {
+
+	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error on initializing db: ", err)
+	}
+	ctx := context.Background()
+
+	db.AutoMigrate(&Product{})
+
+	err = gorm.G[Product](db).Create(ctx, &Product{Code: "D42", Price: 100})
+	
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error on create product", err)	
+	}
+
 	router := gin.Default()
 	api := router.Group("/api")
 	api.GET("/", func(c *gin.Context) {
